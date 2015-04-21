ad1c6933efb6c2dd79e1b0a80439476b26f2a470
Mateus <mateuslunacc@gmail.com>
2015-04-21 00:36:39 -0300

    Refatorei o código

--- dinamica/dynamic.js
+++ dinamica/dynamic.js
@@ -1,22 +1,19 @@
 $(document).ready(function() {
 
-
     $(".form-horizontal").submit(function(event) {
         if($("input:first").val().length > 0){
+            var inputFromForm = $(".form-control").val();
+
             if($("#para-estudar").is(":checked")){
                 $("#assuntos-para-estudar").append(
-                    "<tr><td class='warning'>" +  $(".form-control").val() + "</td></tr>");
+                    "<tr><td class='warning'>" + inputFromForm + "</td></tr>");
             } else {
                 $("#assuntos-estudados").append(
-                    "<tr><td class='success'>" +  $(".form-control").val() + "</td></tr>");
+                    "<tr><td class='success'>" + inputFromForm + "</td></tr>");
             }
             event.preventDefault();
         }
 
     });
-
-    //$("#addtolist").on("click", function() {
-
-
 
 });
